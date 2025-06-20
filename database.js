58e05315320fec17179080fe78c074aa7e44757a
lillabbbbbb <b.lilla6279@gmail.com>
2025-06-20 15:16:06 +0300

    first commit

---
+++ database.js
@@ -0,0 +1,9 @@
+const submitButton = document.getElementById(submit-data)
+
+submitButton.addEventListener("clicked", () => {
+    const username = document.getElementById("input-username")
+    const email = document.getElementById("input-email")
+    const isAdmin = document.getElementById("input-admin")
+    const imageFile = document.getElementById("input-image")
+
+})
